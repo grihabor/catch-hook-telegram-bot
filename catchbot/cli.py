13fc6dae10aa2e0739e952a4129fe953fd2e6ba7
Borodin Gregory <grihabor@gmail.com>
2018-02-13 12:28:54 +0300

    fix parser

--- catchbot/cli.py
+++ catchbot/cli.py
@@ -5,7 +5,7 @@ def cli():
     parser = argparse.ArgumentParser(description='Catch hook telegram bot')
     subparsers = parser.add_subparsers()
 
-    subparsers.add_subparsers('start', help='Start the bot')
-    subparsers.add_subparsers('stop', help='Stop the bot')
+    subparsers.add_parser('start', help='Start the bot')
+    subparsers.add_parser('stop', help='Stop the bot')
 
 
