3a11103a1acf557f502f7f31b1ac1ecf33635b3d
Borodin Gregory <grihabor@gmail.com>
2018-02-13 16:29:46 +0300

    send to all registered users

--- catchbot/routes/__init__.py
+++ catchbot/routes/__init__.py
@@ -4,16 +4,23 @@ from flask import request, jsonify
 from ..catchbot import CatchBot
 
 
+def construct_message_for_user(json_obj):
+    return '```' + json.dumps(json_obj, indent=2) + '```'
+
+
 def register_routes(app: CatchBot):
     @app.route('/', methods=['GET', 'POST'])
     def root():
         if request.method == 'POST':
             if not request.is_json:
                 return jsonify(dict(msg='Data must be in json format')), 400
-            app.updater.bot.send_message(
-                205613939,
-                json.dumps(request.get_json(cache=False)),
-            )
+
+            for chat_id in app.chat_id_list:
+                app.updater.bot.send_message(
+                    chat_id,
+                    construct_message_for_user(request.get_json(cache=False))
+                )
+
             return jsonify(dict(msg='Data successfully sent to user'))
 
         return jsonify(dict(msg='Catch hook telegram bot'))
