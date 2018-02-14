e8b580d46dd0ebd81b2ae000bb82155e50e7b9a4
Borodin Gregory <grihabor@gmail.com>
2018-02-14 17:57:15 +0300

    fix route return

--- catchbot/routes/__init__.py
+++ catchbot/routes/__init__.py
@@ -13,17 +13,19 @@ def _root_post(app):
         for msg in message_list:
             app.updater.bot.send_message(chat_id, msg)
 
-    return jsonify(dict(msg='Data successfully sent to user'))
+    return 'OK', 200
 
 
 def _root_get():
-    return redirect('http://t.me/catch_web_hook_bot')
+    return redirect('http://t.me/catch_web_hook_bot', code=302)
 
 
 def register_routes(app: CatchBot):
     @app.route('/', methods=['GET', 'POST'])
     def root():
         if request.method == 'POST':
-            _root_post(app)
+            return _root_post(app)
         if request.method == 'GET':
-            _root_get()
+            return _root_get()
+
+        return 'Method not allowed', 405
