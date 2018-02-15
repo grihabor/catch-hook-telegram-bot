d68773ebc31a1df228eeb5f84456e8529be113c8
Borodin Gregory <grihabor@gmail.com>
2018-02-15 16:09:30 +0300

    fix start command

--- catchbot/commands/start.py
+++ catchbot/commands/start.py
@@ -1,6 +1,16 @@
+import os
+
+
+def _hook_url(chat_id):
+    return '{protocol}://{host}/hooks/{chat_id}/{hash}'.format(
+        protocol='http',
+        host=os.environ['CATCHBOT_GLOBAL_HOST'],
+        chat_id=chat_id,
+        hash='kljgfvgerf'
+    )
+
 
 def start(bot, update):
-
     bot.send_message(
         chat_id=update.message.chat_id, 
         text='\n'.join([
@@ -8,7 +18,8 @@ def start(bot, update):
             '',
             "I'm the bot to catch your hooks",
             '',
-            'Chat id: {}'.format(update.message.chat_id),
-            'Add this chat id to your server settings',
+            'Send your hooks here: {}'.format(
+                _hook_url(update.message.chat_id)
+            ),
         ]),
     )
