8044bdb65e7531250c1075eb43b3c48dbdbd1e5f
Borodin Gregory <grihabor@gmail.com>
2018-02-15 15:07:38 +0300

    fix env var

--- catchbot/celery_app.py
+++ catchbot/celery_app.py
@@ -8,7 +8,7 @@ def _get_broker():
     return 'pyamqp://{user}:{password}@{host}/{vhost}'.format(
         host='rabbitmq:5672',
         user=os.environ['RABBITMQ_DEFAULT_USER'],
-        password=os.environ['RABBITMQ_DEFAULT_PASSWORD'],
+        password=os.environ['RABBITMQ_DEFAULT_PASS'],
         vhost=os.environ['RABBITMQ_DEFAULT_VHOST'],
     )
 
