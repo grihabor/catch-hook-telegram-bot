d247863ea3053edd90286df08918c18688cfa1f3
Borodin Gregory <grihabor@mail.ru>
2018-02-15 17:52:19 +0300

    use markdown

--- catchbot/tasks.py
+++ catchbot/tasks.py
@@ -7,4 +7,4 @@ bot = RouterBot.from_env()
 
 @app.task
 def send_message_to_bot(chat_id, message):
-    bot.send_message(chat_id, message)
+    bot.send_message(chat_id, message, parse_mode='Markdown')
