92a4532152fd66a476388f15e0dd1676e6882989
Borodin Gregory <grihabor@gmail.com>
2018-02-19 15:49:25 +0300

    use mapping to get info from headers

--- catchbot/routes/header_parser.py
+++ catchbot/routes/header_parser.py
@@ -1,28 +1,24 @@
 from catchbot.config import load_mapping
 
 
-_header_host_mapping = {
-    'X-GitHub-Event': [
-        'github',
-        lambda x: x,
-    ],
-    'X-Gitlab-Event': [
-        'gitlab',
-        lambda x: '_'.join(x.lower().split()[:-1]),
-    ],
-}
-
-
 def get_info_from_headers(headers):
 
-    _event = None
+    _value = None
     _host = None
 
-    for key, (host, mapping) in _header_host_mapping.items():
-        if key not in headers:
+    for host, value in load_mapping()['hosts'].items():
+        header = value['header']
+        if header not in headers:
             continue
-        _event = mapping(headers[key])
+        _value = value
         _host = host
         break
 
-    return {'_event': _event, '_host': _host}
+    result = {
+        'host': _host,
+    }
+    if 'store_header_value' in value:
+        result.update({
+            value['store_header_value']: headers[value['header']]
+        })
+    return result
