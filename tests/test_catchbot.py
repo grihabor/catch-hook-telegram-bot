5de79ec5f8b1335755d44018208f3e0ddaf69385
Borodin Gregory <grihabor@gmail.com>
2018-02-19 12:54:19 +0300

    test mappinh

--- tests/test_catchbot.py
+++ tests/test_catchbot.py
@@ -1,3 +1,12 @@
+import yaml
 
-def test_dummy():
-    pass
+
+def test_mapping_yml():
+    path = 'catchbot/msg_templates/mapping.yml'
+    
+    with open(path, 'r') as f:
+        content = yaml.load(f)
+ 
+    print(content)
+    assert 0
+    
