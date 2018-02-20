d887ee147fd4b4ec124725c04fa9c3298b80de4b
Borodin Gregory <grihabor@gmail.com>
2018-02-20 15:42:27 +0300

    tests for dynamic content

--- catchbot/routes/content/dynamic.py
+++ catchbot/routes/content/dynamic.py
@@ -4,9 +4,9 @@ def get_dynamic_msg_content(mapping, static_content, loaders):
 
     for key, value in mapping.items():
         if not isinstance(value, str):
-            result[key] = get_dynamic_msg_content(value, static_content)
+            result[key] = get_dynamic_msg_content(value, static_content, loaders)
             continue
 
-        result[key] = loaders[value]
+        result[key] = loaders[value](static_content)
 
     return result
--- tests/test_dynamic_content.py
+++ tests/test_dynamic_content.py
@@ -1,7 +1,13 @@
 import pytest
 
-@pytest.mark.parametrize()
-def test_dynamic_content():
+@pytest.mark.parametrize('mapping,static_content,loaders,expected', [
+    ({}, {}, {}, {}),
+    ({'branch': 'get_branch'},
+     {'ref': 'refs/heads/master'},
+     {'get_branch': lambda content: content['ref'].split('/')[-1]},
+     {'branch': 'master'}),
+])
+def test_dynamic_content(mapping, static_content, loaders, expected):
     from catchbot.routes.content import get_dynamic_msg_content
 
-    # get_dynamic_msg_content
+    assert expected == get_dynamic_msg_content(mapping, static_content, loaders)
