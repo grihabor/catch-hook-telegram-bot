841db87a90a8ab96893336bfd53040873db80316
Borodin Gregory <grihabor@gmail.com>
2018-02-20 16:26:39 +0300

    split create msg function

--- catchbot/message/message.py
+++ catchbot/message/message.py
@@ -21,27 +21,31 @@ def _get_template_path(json_obj):
     return path
     
 
-def _render_template(json_obj):
-    with open(_get_template_path(json_obj), 'r') as f:
+def _render_template(json_obj, path):
+    with open(path, 'r') as f:
         template = f.read()
     
     return template.format(**json_obj)
     
 
-def create_message_for_user(headers, json_obj, limit=4096):
+def _get_msg_content(headers, json_obj):
 
     json_obj.update(get_info_from_headers(headers))
 
     mapping = load_mapping()
     host = json_obj['host']
 
-    msg_content = get_message_content_for_user(
+    return get_message_content_for_user(
         json_obj,
         static_mapping=mapping['hosts'][host]['static'],
         dynamic_mapping=mapping['dynamic'],
     )
 
-    msg = _render_template(msg_content)
+
+def create_message_for_user(headers, json_obj, limit=4096):
+    msg_content = _get_msg_content(headers, json_obj)
+    path = _get_template_path(msg_content)
+    msg = _render_template(msg_content, path)
     return (
         msg
         if len(msg) < limit
