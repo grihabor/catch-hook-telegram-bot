c091fd5bc8487f2bc6b0323503158482caffb992
Borodin Gregory <grihabor@gmail.com>
2018-03-06 00:26:51 +0300

    copy github hooks

---
+++ tests/test_hooks.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+_github_url = 'https://developer.github.com/v3/activity/events/types/'
+
+
+def load_github_samples():
+    r = requests.get(_github_url)
+    soup = BeautifulSoup(r.content)
+    code_samples = soup.find_all('code')
+
+    text_samples = (
+        sample.text
+        for sample
+        in code_samples
+    )
+    
+    prev = text_samples[0]
+    for sample in text_samples[1:]:
+        if sample.strip().startswith('{'):
+            content = json.loads(sample)
+            headers = {'': prev}
+            yield headers, content
+        
+        prev = sample
+        
+
+def test_github_hooks():
+    load_github_samples()
+    assert 0
+    
