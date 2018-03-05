2704ac08a70110c81294e1017544448454612dc6
Borodin Gregory <grihabor@gmail.com>
2018-03-06 00:25:05 +0300

    iter github samples

--- tests/test_github_hooks.py
+++ tests/test_github_hooks.py
@@ -6,11 +6,28 @@ from pprint import pprint
 _url = 'https://developer.github.com/v3/activity/events/types/'
 
 
-def test_github_hooks():
+def load_github_samples():
     r = requests.get(_url)
     soup = BeautifulSoup(r.content)
     code_samples = soup.find_all('code')
 
-    pprint([sample.text for sample in code_samples])
+    text_samples = (
+        sample.text
+        for sample
+        in code_samples
+    )
+    
+    prev = text_samples[0]
+    for sample in text_samples[1:]:
+        if sample.strip().startswith('{'):
+            content = json.loads(sample)
+            headers = {'': prev}
+            yield headers, content
+        
+        prev = sample
+        
+
+def test_github_hooks():
+    load_github_samples()
     assert 0
     
