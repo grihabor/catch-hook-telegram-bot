0c572bcb4a63453e05bffec008113e7c99267628
Borodin Gregory <grihabor@gmail.com>
2018-03-05 22:23:25 +0300

    find code samples

--- tests/test_github_hooks.py
+++ tests/test_github_hooks.py
@@ -1,11 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
+
 
 _url = 'https://developer.github.com/v3/activity/events/types/'
 
 
 def test_github_hooks():
     r = requests.get(_url)
-    
-    print(r.content)
+    soup = BeautifulSoup(r.content)
+    code_samples = soup.find_all('code')
+
+    print(code_samples)
     assert 0
     
