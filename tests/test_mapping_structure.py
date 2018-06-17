d07149f13eea6b1703a270db907f52b2fafa9508
Borodin Gregory <grihabor@gmail.com>
2018-06-17 20:50:31 +0300

    Use pkg_resources module to load resources

--- tests/test_mapping_structure.py
+++ tests/test_mapping_structure.py
@@ -1,11 +1,10 @@
 import yaml
+from pkg_resources import resource_string
 
 
 def test_mapping_yml():
-    path = 'catchbot/msg_templates/mapping.yml'
+    path = resource_string('catchbot', 'msg_templates/mapping.yml')
     
     with open(path, 'r') as f:
-        content = yaml.load(f)
- 
-    print(content)
+        assert yaml.load(f)
     
