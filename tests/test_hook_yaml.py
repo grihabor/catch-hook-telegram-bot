584ea4d7ee4755f638bf3372fbddd4e125c63dba
Borodin Gregory <grihabor@gmail.com>
2018-06-19 21:08:32 +0300

    Fix hook yaml test

--- tests/test_hook_yaml.py
+++ tests/test_hook_yaml.py
@@ -3,6 +3,8 @@ import yaml
 
 
 def test_hook_yaml():
-    with resource_stream('catchbot', 'etc/hook.yaml') as f:
-        hook_tree = yaml.load(f)
+	import catchbot
+
+    with resource_stream(catchbot.__name__, 'etc/hook.yaml') as f:
+        obj = yaml.load(f)
 
