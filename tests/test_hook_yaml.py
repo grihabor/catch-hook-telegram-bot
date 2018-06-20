04a98e14fa402ba6a26faa2fa712a01c99e14def
Borodin Gregory <grihabor@gmail.com>
2018-06-20 11:02:47 +0300

    Test catchbot.hook.tree

--- tests/test_hook_yaml.py
+++ tests/test_hook_yaml.py
@@ -1,10 +1,25 @@
 from pkg_resources import resource_stream
 import yaml
+import pytest
 
 
 def test_hook_yaml():
     import catchbot
 
     with resource_stream(catchbot.__name__, 'etc/hook.yaml') as f:
-        obj = yaml.load(f)
+        assert yaml.load(f)
 
+
+def test_hook_tree_load():
+	from catchbot import hook
+	
+	assert hook.tree.load()
+	
+
+@pytest.mark.parametrize('hook_tree', [])
+def test_hook_tree_validate_errors(hook_tree):
+	from catchbot import hook
+	
+	with pytest.raises(hook.tree.HookTreeError):
+		hook.tree.validate(hook_tree)
+	
